86e1cd5a9bb18ade0ee1a4735c079e67a1b92b5f
immortalvoyage <immortalvoyage@gmail.com>
2026-08-08 22:50:03 +0800

    fix: 公告改為獨立訊息
    * fix: 公告改為獨立訊息
    
    * test: 公告同頻道仍須獨立發送

--- tests/discord-cross-post.test.js
+++ tests/discord-cross-post.test.js
@@ -2,13 +2,16 @@ import test from "node:test";
 import assert from "node:assert/strict";
 import { attachMasterCrossPost, extractCrossPostChannelId } from "../src/integrations/discord-cross-post.js";
 
-test("宗主要求把訊息發到另一個 Discord 頻道時可解析目標頻道", () => {
+test("宗主要求把訊息發到指定 Discord 頻道時可解析目標頻道", () => {
   assert.equal(
     extractCrossPostChannelId("訊息要發到 <#1531897588687704116> 不然他們看不到", "1534238116099919933"),
     "1531897588687704116"
   );
   assert.equal(extractCrossPostChannelId("請在這裡回覆 <#1531897588687704116>", "1534238116099919933"), "");
-  assert.equal(extractCrossPostChannelId("發到 <#1534238116099919933>", "1534238116099919933"), "");
+  assert.equal(
+    extractCrossPostChannelId("幫我發個公告到 <#1534238116099919933>", "1534238116099919933"),
+    "1534238116099919933"
+  );
 });
 
 test("只有宗主能取得跨頻道發送動作", () => {
